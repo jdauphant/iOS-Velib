3e160a4c9cef734e44e41c4d01ef3c246ea5806c
Julien DAUPHANT <judauphant@gmail.com>
2015-07-26 01:03:42 +0200

    Connect with WebSocket

--- iOS-Velib/ViewController.swift
+++ iOS-Velib/ViewController.swift
@@ -8,7 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, SRWebSocketDelegate  {
+    @IBOutlet weak var textView: UITextView!
+    
+    private let socketURL = "ws://reactivevelib-env.elasticbeanstalk.com/socket"
+    private var socket: SRWebSocket?
+    
+    @IBAction func connect() {
+        socket = SRWebSocket(URL: NSURL(string: socketURL))
+        socket?.delegate = self
+        socket?.open()
+    }
+    
+    private func addText(msg: String) {
+        textView.text = "\(textView.text)\(msg)\n"
+    }
+    
+    func webSocket(webSocket: SRWebSocket!, didReceiveMessage message: AnyObject!) {
+        if let msg = message as? String {
+            addText(msg)
+        }
+    }
+    
+    func webSocketDidOpen(webSocket: SRWebSocket!) {
+        addText("Connected")
+        webSocket.send("[\"subscribeAll\"]")
+    }
 }
 
