5b18e7d16a5692227fe943a17059ec9b2a5d4837
Julien DAUPHANT <judauphant@gmail.com>
2015-07-25 19:19:00 +0200

    First Commit : Empty Project

---
+++ iOS-Velib/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS-Velib
+//
+//  Created by Julien DAUPHANT on 25/07/15.
+//  Copyright (c) 2015 Julien Dauphant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
